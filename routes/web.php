48a6b013f43f6ccb9344dacb8f9fa842980b99c2
ericdourado <ericdourado1@hotmail.com>
2023-02-05 12:04:50 -0300

    antes do breeze

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SeasonController;
 use App\Http\Controllers\SeriesController;
 use App\Http\Controllers\UsersController;
+use App\Http\Middleware\Autenticador;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,13 +19,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/series');
-})->middleware(App\Http\Middleware\Autenticador::class);
-
+Route::get(
+    '/',
+    function () {
+        return redirect('/series');
+    }
+);
 Route::resource('/series', SeriesController::class);
-Route::get('/series/{series}/seasons', [SeasonController::class, 'index'])->name('seasons.index');
-Route::get('/season/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+Route::middleware('autenticador')->group(function () {
+    Route::get('/series/{series}/seasons', [SeasonController::class, 'index'])->name('seasons.index');
+    Route::get('/season/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+});
+
+
+
+
+
+
 Route::post('/season/{season}/episodes', [EpisodesController::class, 'update']);
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('sign');
